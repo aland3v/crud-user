031f46d9986011532bb288af0a5e8a97521911bd
Alan dev <aland3v@gmail.com>
2021-02-27 10:29:42 -0400

    un crud basico con laravel

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 mx-auto">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    - {{ $error }} <br />
+                                @endforeach
+                            </div>
+                        @endif
+                        <form action="{{ route('users.store') }}" method="POST">
+                            <div class="form-row">
+                                <div class="col-sm-3">
+                                    <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{ old('name') }}">
+                                </div>
+                                <div class="col-sm-4">
+                                    <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="password" name="password" class="form-control" placeholder="Contraseña">
+                                </div>
+                                <div class="col-sm-auto">
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <form action="{{ route('users.destroy', $user) }}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <input 
+                                            type="submit" 
+                                            value="Eliminar" 
+                                            class="btn btn-sm btn-danger"
+                                            onclick="return confirm('¿Desea Eliminar... ?')"
+                                            >
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
